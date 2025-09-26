420aab75c1a537f33aa446975e7bde30cb097378
lihaibo <15039069378@163.com>
2025-09-26 17:40:46 +0800

    feat: 资源整合

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,14 @@
 <template>
-  <div>Hello -world</div>
-  <p>{{ userName }}</p>
-  <box />
+  <div class="flex-center  h-[100vh]">
+    <h1 class="text-3xl text-indigo-300">Hello,GSAP!</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from "pinia";
-import { useUserStore } from "@/piniaStore";
-import box from "@/components/box.vue";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
 defineOptions({
   name: "Home",
 });
-
-const store = useUserStore();
-
-const { userName } = storeToRefs(store);
 </script>
 <style scoped></style>
