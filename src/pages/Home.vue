2446476c147f96f69bd72fa769223a8f9380f052
lihaibo <15039069378@163.com>
2025-09-26 12:05:28 +0800

    feat: 初始化pinia仓库

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,17 @@
 <template>
   <div>Hello -world</div>
+  <p>{{ userName }}</p>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useUserStore } from "@/piniaStore";
 defineOptions({
   name: "Home",
 });
+
+const store = useUserStore();
+
+const { userName } = storeToRefs(store);
 </script>
 <style scoped lang="scss"></style>
